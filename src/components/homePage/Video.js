457c81c07bece5604a139e1659879e7a67f8d663
Alex Friedman <alexbfriedman@googlemail.com>
2021-02-06 11:44:14 +0800

    fix: added placeholder poster to video

--- src/components/homePage/Video.js
+++ src/components/homePage/Video.js
@@ -1,11 +1,21 @@
 import React from "react";
 
 import TestVideo from "../../assets/video/intro-video.mp4";
+import testImage from "../../assets/images/Dance Page/jazz.jpg";
 import classes from "../../styles/pages/homePage/video.module.scss";
 
 const Video = () => {
   return (
-    <video autoPlay muted loop controls disablePictureInPicture playsInline="playsInline" className={classes.video}>
+    <video
+      autoPlay
+      muted
+      loop
+      controls
+      disablePictureInPicture
+      playsInline="playsInline"
+      poster={testImage}
+      className={classes.video}
+    >
       <source src={TestVideo} type="video/mp4" />
       <p>Your browser doesn't support HTML5 video</p>
     </video>
