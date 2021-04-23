9c4718d69a7773a24b11104a08d77c293f063fd1
Alex Friedman <alexbfriedman@googlemail.com>
2021-04-23 12:56:00 +0800

    fix: affiliate logo styles

--- src/constants/affLogos.js
+++ src/constants/affLogos.js
@@ -37,48 +37,45 @@ const About = () => {
     <div className={classes.affLinksContainer}>
       <ul className={classes.affLinks__list}>
         <li className={classes.affLinks__listItem}>
-          <a href="https://www.facebook.com/cmm.1998ph/">
+          <a
+            className={classes.affLinks__anchor}
+            href="https://www.facebook.com/cmm.1998ph/"
+          >
             <Img
               fixed={data.cmm.childCloudinaryAsset.fixed}
               alt="logo of The Center of Movement and Music"
               className={classes.affLinks__logo}
             />
-          </a>
-          <a
-            className={classes.affLinks__textLink}
-            href="https://www.facebook.com/cmm.1998ph/"
-          >
-            Center for Movement and Music
+            <p href="https://www.facebook.com/cmm.1998ph/">
+              Center for Movement and Music
+            </p>
           </a>
         </li>
         <li className={classes.affLinks__listItem}>
-          <a href="https://www.abrsm.org/">
+          <a className={classes.affLinks__anchor} href="https://www.abrsm.org/">
             <Img
               fixed={data.abrsm.childCloudinaryAsset.fixed}
               alt="logo of The Associated Board of the Royal Schools of Music"
               className={classes.affLinks__logo}
             />
-          </a>
-          <a
-            className={classes.affLinks__textLink}
-            href="https://www.abrsm.org/"
-          >
-            Associated Board of the Royal Schools of Music (ABRSM)
+
+            <p href="https://www.abrsm.org/">
+              Associated Board of the Royal Schools of Music (ABRSM)
+            </p>
           </a>
         </li>
         <li className={classes.affLinks__listItem}>
-          <a href="http://www.acbaustralia.com.au/">
+          <a
+            className={classes.affLinks__anchor}
+            href="http://www.acbaustralia.com.au/"
+          >
             <Img
               fixed={data.acb.childCloudinaryAsset.fixed}
               alt="logo of Australian based Ballet Conservatoire"
               className={classes.affLinks__logo}
             />
-          </a>
-          <a
-            className={classes.affLinks__textLink}
-            href="http://www.acbaustralia.com.au/"
-          >
-            Ballet Conservatoire
+
+            <p href="http://www.acbaustralia.com.au/">Ballet Conservatoire</p>
           </a>
         </li>
       </ul>
