60ec2a131e79b94b5c00eb39e020735e399a4ad7
Alex Friedman <alexbfriedman@googlemail.com>
2021-05-24 17:24:03 +0800

    commented out alwaysUseDefaultBase64 in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
         apiKey: process.env.GATSBY_CLOUDINARY_API_KEY,
         apiSecret: process.env.GATSBY_CLOUDINARY_API_SECRET,
         uploadFolder: "gatsby-cloudinary",
-        alwaysUseDefaultBase64: true,
+        // alwaysUseDefaultBase64: true, // Make sure insta photos still load on mobile devices
         enableDefaultTransformations: true,
       },
     },
