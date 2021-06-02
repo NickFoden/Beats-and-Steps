45407e4c888b1b1c4516d5d42b2c663d73970285
Alex Friedman <alexbfriedman@googlemail.com>
2021-06-02 20:31:11 +0800

    fixed sidebar menu being small on tablet landscape mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,10 @@ require("dotenv").config({
 });
 
 /* *** THIS WAS FROM GATSBY ROBOTS DOC, CODE BELOW IS FOR PREVENTING branch and previews from being crawled *** */
+// NOTE - NODE_ENV is a reserved gatsby env var (so don't need to define), which specifies whether in dev or prod mode
 // const {
 //   NODE_ENV,
-//   URL: NETLIFY_SITE_URL = "https://www.example.com",
+//   URL: NETLIFY_SITE_URL = "https://www.example.com", // Note, no trailing slash
 //   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
 //   CONTEXT: NETLIFY_ENV = NODE_ENV,
 // } = process.env;
@@ -47,12 +48,38 @@ module.exports = {
     description: `The Beats and Steps Arts Academy is a CMM affiliate center that envisions developing children's full potential in the arts.`,
     author: `Alex Friedman`,
     image: `/logo.png`,
-    // siteUrl: // *siteURL goes here but remove the trailing forward slash*,
+    // siteUrl, // *siteURL goes here but remove the trailing forward slash* - If using the robots stuff above, just leave it as siteUrl,
   },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-sitemap`,
+    // `gatsby-plugin-sitemap`, // Read the plugin page, it needs to be configured - https://github.com/gatsbyjs/gatsby/issues/8946 & https://joshuatz.com/posts/2019/gatsby-better-last-updated-or-modified-dates-for-posts/
+    // Actually try it "default" to start and see what it says
+    //
+    // *** ROBOTS-TXT CONFIGURED FOR NETLIFY - ENABLE THIS ONCE DOMAIN NAME IS BOUGHT ***
+    //     {
+    //       resolve: "gatsby-plugin-robots-txt",
+    //       options: {
+    //         resolveEnv: () => NETLIFY_ENV,
+    //         env: {
+    //           production: {
+    //             policy: [{ userAgent: "*" }],
+    //           },
+    //           "branch-deploy": {
+    //             policy: [{ userAgent: "*", disallow: ["/"] }],
+    //             sitemap: null,
+    //             host: null,
+    //           },
+    //           "deploy-preview": {
+    //             policy: [{ userAgent: "*", disallow: ["/"] }],
+    //             sitemap: null,
+    //             host: null,
+    //           },
+    //         },
+    //       },
+    //     },
+    // 
+    // *** STANDARD WAY OF DOING THE ROBOTS PLUGIN, DELETE IF ABOVE WORKS
     // {
     //   resolve: `gatsby-plugin-robots-txt`,
     //   options: {
@@ -68,7 +95,7 @@ module.exports = {
         apiKey: process.env.GATSBY_CLOUDINARY_API_KEY,
         apiSecret: process.env.GATSBY_CLOUDINARY_API_SECRET,
         uploadFolder: "gatsby-cloudinary",
-        // alwaysUseDefaultBase64: true, // Make sure insta photos still load on mobile devices
+        // alwaysUseDefaultBase64: true,
         enableDefaultTransformations: true,
       },
     },
@@ -100,14 +127,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // NEED TO SET UP GRAPH-API OPTION
     {
       resolve: `gatsby-source-instagram`,
       options: {
         username: `1503767980`,
-        // This new long-lived access token will expire on July 25, 2021:
-        access_token:
-          process.env.GATSBY_INSTAGRAM_TOKEN,
+        // This new long-lived access token will expire on July 25, 2021: AFTER REFRESH DON'T FORGET TO CHANGE NETLIFY ENV VARIABLE
+        access_token: process.env.GATSBY_INSTAGRAM_TOKEN,
         instagram_id: process.env.GATSBY_INSTA_BUSINESS_ID,
         paginate: 6,
         maxPosts: 6,
