355bbec04ad0cdf9c6a4c814bebee9db0bce5119
Alex Friedman <alexbfriedman@googlemail.com>
2021-06-04 12:03:17 +0800

    fix: iphone vid gap

--- src/components/navigation/CustomerChat.js
+++ src/components/navigation/CustomerChat.js
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-const facebookAppId = process.env.GATSBY_FB_APP_ID;
+// const facebookAppId = process.env.GATSBY_FB_APP_ID;
 
-function CustomerChat() {
-  useEffect(() => {
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        xfbml: true,
-        version: "v10.0",
-      });
-    };
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  });
-  return (
-    <>
-      <div id="fb-root" />
-      <div
-        className="fb-customerchat"
-        attribution="page_inbox"
-        page_id={facebookAppId}
-      />
-    </>
-  );
-}
+// function CustomerChat() {
+//   useEffect(() => {
+//     window.fbAsyncInit = function () {
+//       window.FB.init({
+//         xfbml: true,
+//         version: "v10.0",
+//       });
+//     };
+//     (function (d, s, id) {
+//       var js,
+//         fjs = d.getElementsByTagName(s)[0];
+//       if (d.getElementById(id)) return;
+//       js = d.createElement(s);
+//       js.id = id;
+//       js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+//       fjs.parentNode.insertBefore(js, fjs);
+//     })(document, "script", "facebook-jssdk");
+//   });
+//   return (
+//     <>
+//       <div id="fb-root" />
+//       <div
+//         className="fb-customerchat"
+//         attribution="page_inbox"
+//         page_id={facebookAppId}
+//       />
+//     </>
+//   );
+// }
 
-export default CustomerChat;
+// export default CustomerChat;
 
 
 
