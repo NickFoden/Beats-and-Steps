83abff889922fbcfb733cbe4fc51f7a40582bd7a
Alex Friedman <alexbfriedman@googlemail.com>
2021-06-06 21:58:43 +0800

    deleted redundant comment

--- wrapPageElement.js
+++ wrapPageElement.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import CustomerChat from "./src/components/navigation/CustomerChat";
 
-// Pass all props (hence the ...props) to the layout component so it has access to things like pageContext or location
 const wrapPageElement = ({ element }) => (
   <div>
     {element}
